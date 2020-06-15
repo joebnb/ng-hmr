023d9b286968d339525c7a7a0c9715729112dfc5
chenbo <joebnb@qq.com>
2020-06-15 16:57:19 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,13 @@ type AppRef = any & {
 
 export function enableNgHmr(module: HotModuleRef) {
 
-    return function (applicationRef: AppRef, bootstrap: HmrOption, PagesModule) {
+    return function (applicationRef: AppRef, bootstrap: HmrOption) {
         let ngModule: NgModuleRef<any>;
         bootstrap().then(mod => ngModule = mod);
         module.hot.accept((e: any) => { });
         module.hot.dispose(() => {
             const appRef: AppRef = ngModule.injector.get(applicationRef);
-            const elements = appRef.components.map(component => component.location.nativeElement);
+            const elements = appRef.components.map((component: any) => component.location.nativeElement);
             const render = createNewApplication(elements);
             ngModule.destroy();
             render();
